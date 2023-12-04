5e44719875a7e3b9e7f2a42c72b9cd70287b11c7
Peter Lord <github@ptrlrd.com>
2023-12-03 20:53:58 -0800

    Update palette.lua

--- lua/draculapro/palette.lua
+++ lua/draculapro/palette.lua
@@ -1,29 +1,29 @@
 return {
   base = {
     fg = '#F8F8F2',
-    cyan = '',
-    green = '',
-    orange = '',
-    pink = '',
-    purple = '',
-    red = '',
-    yellow = '',
-    sign_add = '', -- green
-    sign_changes = '', -- orange
-    sign_delete = '', -- red or purple
-    info = '', -- cyan
-    hint = '', -- cyan
-    error = '', -- red or purple
-    warning = '', -- orange
-    other = '', -- purple or cyan
+    cyan = '#80FFEA',
+    green = '#8AFF80',
+    orange = '#FFCA80',
+    pink = '#FF80BF',
+    purple = '#9580FF',
+    red = '#FF9580',
+    yellow = '#FFFF80',
+    sign_add = '#8AFF80', -- green
+    sign_changes = '#FFCA80', -- orange
+    sign_delete = '#FF9580', -- red or purple
+    info = '#80FFEA', -- cyan
+    hint = '#80FFEA', -- cyan
+    error = '#FF9580', -- red or purple
+    warning = '#FFCA80', -- orange
+    other = '#80FFEA', -- purple or cyan
   },
   pro = {
-    comment = '',
-    selection = '',
-    currentline = '', -- selection == currentline
+    comment = '#',
+    selection = '#454158',
+    currentline = '#454158', -- selection == currentline
     bglighter = '', -- optional
     bglight = '', -- optional
-    bg = '',
+    bg = '#22212C',
     bgdark = '',
     bgdarker = '',
   },
